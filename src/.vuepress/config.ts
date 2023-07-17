b353b3745fd5f2b4996236fd5ee04d343a6fa36c
jact <jachavar@gmail.com>
2023-07-17 13:33:39 +0200

    fix: base setting

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { searchPlugin } from '@vuepress/plugin-search'
 import theme from './theme.js'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/azlinux.github.io/',
 
   lang: 'es-ES',
   title: 'AZLinux',
